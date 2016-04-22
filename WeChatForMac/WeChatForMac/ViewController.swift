ed15f77aabc63700a3d63d9832aaa0a0911acf19
liangmingzou@163.com <liangmingzou@163.com>
2016-04-22 15:29:15 +0800

    Add WeChat demo

---
+++ WeChatForMac/WeChatForMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeChatForMac
+//
+//  Created by bruce on 16/4/22.
+//  Copyright © 2016年 ZouLiangming. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
